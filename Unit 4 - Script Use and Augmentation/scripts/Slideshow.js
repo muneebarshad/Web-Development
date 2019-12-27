bcc07e9c53f1a0492d05c3821141ede643747935
muneebarshad <muneeb.arshad@hotmail.com>
2019-12-26 22:14:43 -0500

    Slideshow home page created

--- Unit 4 - Script Use and Augmentation/scripts/Slideshow.js
+++ Unit 4 - Script Use and Augmentation/scripts/Slideshow.js
@@ -2,17 +2,19 @@
 Link to guide: https://www.w3schools.com/howto/howto_js_slideshow.asp
 Fair Use Policy: https://www.w3schools.com/about/default.asp */
 
-
+var timer = null;
 var slideIndex = 1;
 var slides = document.getElementsByClassName("mySlides");
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
+  clearTimeout(timer);
   showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
+  clearTimeout(timer);
   showSlides(slideIndex = n);
 }
 
@@ -27,4 +29,25 @@ function showSlides(n) {
   }
 
   slides[slideIndex-1].style.display = "block";
+
+}
+
+//---------------------------------------------------
+
+
+var slideindex = 0;
+autoshowSlides();
+
+function autoshowSlides() {
+    var i = 0;
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideindex++;
+    if (slideindex > slides.length) {
+        slideindex = 1;
+    }
+    slides[slideindex - 1].style.display = "block";
+    setTimeout(autoshowSlides, 5000);
 }
