449725c32231162e33fd227df5b5467d1b304eae
muneebarshad <muneeb.arshad@hotmail.com>
2020-04-04 20:01:52 -0400

    Unit 7 code update

---
+++ Unit 7 - Using External data Sources/scripts/twitter.js
@@ -0,0 +1,42 @@
+/*
+ * This script will add a tweet button to the DOM using the Twitter JavaScript API.
+ */
+
+//The API hasn't loaded yet
+if (typeof (twttr.ready) == "undefined") {
+
+    //Wait until the API is loaded...
+    document.getElementById("twitter-wjs").addEventListener("load", function () {
+        //Wait for all the elements to load in
+        $(document).ready(function () {
+            //Add the Twitter tweet button
+            twttr.widgets.createShareButton(
+              //Product link
+                $(document)[0].URL,
+                document.getElementById("tweet-shareBtn"),
+                {
+                    size: "large",
+                    text: "Check out our latest Goldfix product! Learn about, '" + $('.product-name')[0].innerHTML + "'",
+                    hashtags: "Goldfix!",
+                }
+            );
+
+        });
+    });
+
+} else { //The API loaded already
+
+    console.log("Twitter API Loaded");
+
+    //Add the Twitter tweet button
+    twttr.widgets.createShareButton(
+        $(document)[0].URL,
+        document.getElementById("tweet-container"),
+        {
+            size: "large",
+            text: "Check out our latest Goldfix product! Learn about, '" + $('.product-name')[0].innerHTML + "'",
+            hashtags: "Goldfix!",
+        }
+    );
+
+}
